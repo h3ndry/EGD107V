9c39ee05464912e215617657f72fb97d941408fe
hendry <h3ndry@users.noreply.github.com>
2024-03-12 22:51:21 +0200

    feat(gpio): control LED

--- virtual-fence-esp32/main/virtual-fence-esp32.c
+++ virtual-fence-esp32/main/virtual-fence-esp32.c
@@ -1,6 +1,23 @@
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "hal/gpio_types.h"
+#include "soc/gpio_num.h"
+#include <stdbool.h>
 #include <stdio.h>
 
-void app_main(void)
-{
+void app_main(void) {
 
+  gpio_set_direction(GPIO_NUM_22, GPIO_MODE_INPUT);
+  gpio_set_pull_mode(GPIO_NUM_22, GPIO_PULLUP_ONLY);
+  gpio_set_direction(GPIO_NUM_26, GPIO_MODE_OUTPUT);
+
+  while (true) {
+    if (gpio_get_level(GPIO_NUM_22))
+      gpio_set_level(GPIO_NUM_26, 1);
+    else
+      gpio_set_level(GPIO_NUM_26, 0);
+
+    vTaskDelay(10);
+  }
 }
