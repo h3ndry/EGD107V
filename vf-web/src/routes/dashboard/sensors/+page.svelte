8ab2778021ad84541b7bd82994851b012a7af6e7
hendry <h3ndry@users.noreply.github.com>
2024-03-25 12:52:20 +0200

    style(centre):  centre LED button

--- vf-web/src/routes/dashboard/sensors/+page.svelte
+++ vf-web/src/routes/dashboard/sensors/+page.svelte
@@ -2,12 +2,10 @@
 	import { Button } from '$lib/components/ui/button';
 </script>
 
+<div class="col-span-3 lg:col-span-4 flex flex-col justify-center lg:border-l">
+	<div class="mx-auto w-36">
+		<svg class="bor h-24 w-24 rounded-full bg-red-800 mb-4"> </svg>
 
-
-<div class='items-center flex'>
-	<svg class="bg-red-800 rounded-full h-24 w-24 bor">
-
-	</svg>
-
-<Button > Switc on LED </Button>
+		<Button>Switc on LED</Button>
+	</div>
 </div>
